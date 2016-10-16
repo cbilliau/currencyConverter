981501ec13e1cff317b73035ba8c140c3da041b9
cbilliau <christopher@billiau.com>
2016-10-16 12:41:29 -0400

    Successful rewrite of signup for new user using bcrypt to hash password and create user account in db.

--- server.js
+++ server.js
@@ -17,26 +17,126 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+app.use(passport.initialize());
+
+// strategy
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function(err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
+    });
+});
+passport.use(strategy);
 
 // routes
 app.post('/signup', function(req, res) {
     console.log('signup');
+
+    if (!req.body) {
+        return res.status(400).json({
+            message: "No request body"
+        });
+    }
+
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
+        });
+    }
+
     var username = req.body.username;
+
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+
+    username = username.trim();
+
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+
     var password = req.body.password;
-    var user = new User({username: username, password: password});
-    var currency = new Currency({username: username, userCurrencies: []});
-    user.save().then(function() {
-        console.log("successfully saved user!");
-        return currency.save();
-    }, function(err) {
-        console.log("error saving user! " + err);
-        return res.status(201).json({success: false});
-    }).then(function() {
-        console.log("successfully saved currency data for user !");
-        res.status(201).json({success: true});
-    }, function(err) {
-        res.status(201).json({success: false});
-        console.log("error saving currency for user!" + err);
+
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+    // Ecnryption
+    // Generate salt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash,
+
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).json({success: false});
+                }
+
+                return res.status(201).json({success: true});
+                console.log("successfully saved currency data for user !");
+
+            });
+        });
     });
 });
 
