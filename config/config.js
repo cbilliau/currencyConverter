de979b0c86f44a28396a9e470fecc4fa44f03c9b
cbilliau <christopher@billiau.com>
2016-10-27 18:52:09 -0400

    App deploys but bypasses login and goes directly to main screen. researching solutions through heroku mlab docs. Test5

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-exports.DATABASE_URL = process.env.MONGOLAB_URI ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/current-c' :
-                            'mongodb://localhost/current-c-dev');
+exports.DATABASE_URL = process.env.MONGOLAB_URI || 'mongodb://localhost/current-c';
+                      //  global.DATABASE_URL ||
+                      //  (process.env.NODE_ENV === 'production' ?
+                      //       'mongodb://localhost/current-c' :
+                      //       'mongodb://localhost/current-c-dev');
 
 exports.PORT = process.env.PORT || 8080;
