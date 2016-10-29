fee16fe5a806cd65cc782a199da6b031d720f6ed
Mostafa <m.elsheikh11791@gmail.com>
2016-10-29 12:56:35 +0200

    fixes

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
-exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/current-c';
+exports.DATABASE_URL = "mongodb://chris:chris@ds137197.mlab.com:37197/chriscurrencyconverter"
+                        //process.env.MONGODB_URI || 'mongodb://localhost/current-c';
                       //  global.DATABASE_URL ||
                       //  (process.env.NODE_ENV === 'production' ?
                       //       'mongodb://localhost/current-c' :
